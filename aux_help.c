118a9ff45d651e510cec1d8bbd3bd4568636ff25
mike489 <michaeltesfaye84@gmail.com>
2022-06-15 22:33:07 +0300

    edit

--- aux_help.c
+++ aux_help.c
@@ -71,9 +71,9 @@ void aux_help_general(void)
 void aux_help_exit(void)
 {
 	char *help = "exit: exit [n]\n Exit shell.\n";
-[O
+
 	write(STDOUT_FILENO, help, _strlen(help));
-[I	help = "Exits the shell with a status of N. If N is ommited, the exit";
+	help = "Exits the shell with a status of N. If N is ommited, the exit";
 	write(STDOUT_FILENO, help, _strlen(help));
 	help = "statusis that of the last command executed\n";
 	write(STDOUT_FILENO, help, _strlen(help));
